94822e186885d5857c34a0ecd257d2b2e1f88736
EdenHubs12 <35420387+EdenHubs12@users.noreply.github.com>
2018-07-08 14:49:34 +0800

    Added the KIT and ForLoop basics

---
+++ SimpleCalculator (minus).py
@@ -0,0 +1,3 @@
+x = int (input("Enter a number: ") )
+y = int (input("Enter second number: ") )
+print ('The division awnser of ', x, ' and ', y, ' is ', x-y, '.', sep='')
---
+++ SimpleCalculator (multiplied).py
@@ -0,0 +1,3 @@
+x = int (input("Enter a number: ") )
+y = int (input("Enter second number: ") )
+print ('The multiplied awnser of ', x, ' and ', y, ' is ', x*y, '.', sep='')
